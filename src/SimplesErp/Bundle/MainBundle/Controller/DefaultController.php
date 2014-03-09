ce6f294e06bc71ad0d4618865965d7e0f2b89832
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-03-09 10:39:16 -0300

    primeiro commit. Symfony inicial

---
+++ src/SimplesErp/Bundle/MainBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SimplesErp\Bundle\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SimplesErpMainBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/SimplesErp/Bundle/MainBundle/SimplesErpMainBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SimplesErp\Bundle\MainBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SimplesErpMainBundle extends Bundle
+{
+}
